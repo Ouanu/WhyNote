6096784effcbd8b0e7cc59b35dc72c7971b91d06
Ouanu <ouanu@foxmail.com>
2021-11-24 21:41:04 +0800

    WhyNote

--- app/src/main/java/com/moment/whynote/MainActivity.java
+++ app/src/main/java/com/moment/whynote/MainActivity.java
@@ -1,10 +1,10 @@
 package com.moment.whynote;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
@@ -19,9 +19,7 @@ import com.moment.whynote.fragment.ResFragment;
 public class MainActivity extends AppCompatActivity {
 
     private ResRepository repository;
-    private FrameLayout flFragment;
-    private Button insertBtn;
-    private MainHandler handler = new MainHandler();
+    private final MainHandler handler = new MainHandler();
     private final static String TAG = "MainActivity";
     private final static int DATABASE_IS_ALREADY = 10000;
 
@@ -29,21 +27,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        getResRepository();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                repository = new ResRepository(MainActivity.this);
-//                repository.insertData();
-                if (ResRepository.getInstance() != null) {
-                    handler.sendEmptyMessage(DATABASE_IS_ALREADY);
-                }
-//
+
+
+    }
+
+    /**
+     * 获取仓库实例
+     */
+    private void getResRepository() {
+        new Thread(() -> {
+            repository = new ResRepository(MainActivity.this);
+            if (ResRepository.getInstance() != null) {
+                handler.sendEmptyMessage(DATABASE_IS_ALREADY);
             }
         }).start();
-        initView();
-
-
     }
 
     /**
@@ -59,39 +59,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 初始化控件
+     */
     private void initView() {
-        flFragment = (FrameLayout) findViewById(R.id.fl_fragment);
-        insertBtn = (Button) findViewById(R.id.insert_btn);
-        insertBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        repository = ResRepository.getInstance();
-                        ResData resData = new ResData();
-                        resData.title = "SAD";
-                        resData.desc = "SAD";
-                        resData.uri = "SDADSADA";
-                        repository.insertData(resData);
-                    }
-                }).start();
-
-            }
-        });
+        FrameLayout flFragment = (FrameLayout) findViewById(R.id.fl_fragment);
+        Button insertBtn = (Button) findViewById(R.id.insert_btn);
+        insertBtn.setOnClickListener(v -> new Thread(() -> {
+            repository = ResRepository.getInstance();
+            ResData resData = new ResData();
+            resData.title = "SAD";
+            resData.desc = "SAD";
+            resData.uri = "SDADSADA";
+            repository.insertData(resData);
+        }).start());
     }
 
+    /**
+     * 线程间通信，按指定顺序完成作业
+     */
+    @SuppressLint("HandlerLeak")
     private class MainHandler extends Handler {
         @Override
         public void handleMessage(@NonNull Message msg) {
-//            super.handleMessage(msg);
-            switch (msg.what) {
-                case DATABASE_IS_ALREADY:
-                    Log.d(TAG, "handleMessage: -------------");
-                    getFragment();
-                    break;
-                default:
-                    break;
+            if (msg.what == DATABASE_IS_ALREADY) {
+                Log.d(TAG, "handleMessage: -------------");
+                getFragment();
             }
         }
     }
