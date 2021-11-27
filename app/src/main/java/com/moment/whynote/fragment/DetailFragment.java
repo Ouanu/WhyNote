1155aed5fce8fcddc8932718b4142eb8234aa558
Ouanu <ouanu@foxmail.com>
2021-11-27 22:44:06 +0800

    WhyNote

--- app/src/main/java/com/moment/whynote/fragment/DetailFragment.java
+++ app/src/main/java/com/moment/whynote/fragment/DetailFragment.java
@@ -7,31 +7,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethod;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.moment.whynote.R;
 import com.moment.whynote.utils.DataUtils;
-
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,15 +30,12 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
     private final static String TAG = "DetailFragment";
     private EditText etDesc;
     private boolean flag = false;
-    private DataUtils utils = new DataUtils();
+    private final DataUtils utils = new DataUtils();
     private RecyclerView recyclerView;
     private List<String> uriList = new ArrayList<>();
-    private UriAdapter adapter;
     private EditText etTitle;
-    private FrameLayout flUri;
-
     private Method method;
-    private Class<EditText> cls = EditText.class;
+    private final Class<EditText> cls = EditText.class;
 
 
     @Nullable
@@ -68,12 +54,12 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
         etTitle = view.findViewById(R.id.et_title);
         etDesc = view.findViewById(R.id.et_desc);
         ImageButton btnGetUrl = view.findViewById(R.id.btn_get_uri);
-        FrameLayout flUri = view.findViewById(R.id.fl_uri);
-        etTitle.setText(bundle.getString("title"));
-        etDesc.setText(bundle.getString("desc"));
+        if (bundle != null) {
+            etTitle.setText(bundle.getString("title"));
+            etDesc.setText(bundle.getString("desc"));
+        }
         etSetOnTouchListener();
         btnGetUrl.setOnClickListener(this);
-        flUri = view.findViewById(R.id.fl_uri);
         try {
             method = cls.getMethod("setShowSoftInputOnFocus", boolean.class);
             method.setAccessible(true);
@@ -91,56 +77,50 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
      */
     @SuppressLint("ClickableViewAccessibility")
     private void etSetOnTouchListener() {
-        etDesc.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == 0) {
-                    flag = true;
-                } else if (event.getAction() == 2) {
-                    //触摸事件：不弹出键盘
-                    flag = false;
-                    try {
-                        method.invoke(etDesc, false);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else if (event.getAction() == 1 && flag) {
-                    //点击事件，弹出键盘
-                    try {
-                        method.invoke(etDesc, true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+        etDesc.setOnTouchListener((v, event) -> {
+            if (event.getAction() == 0) {
+                flag = true;
+            } else if (event.getAction() == 2) {
+                //触摸事件：不弹出键盘
+                flag = false;
+                try {
+                    method.invoke(etDesc, false);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                return false;
+            } else if (event.getAction() == 1 && flag) {
+                //点击事件，弹出键盘
+                try {
+                    method.invoke(etDesc, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            return false;
         });
 
 
         //同上
-        etTitle.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Log.d(TAG, "onTouch: ----" + event.getAction() + "   " + flag);
+        etTitle.setOnTouchListener((v, event) -> {
+            Log.d(TAG, "onTouch: ----" + event.getAction() + "   " + flag);
 
-                if (event.getAction() == 0) {
-                    flag = true;
-                } else if (event.getAction() == 2) {
-                    flag = false;
-                    try {
-                        method.invoke(etTitle, false);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else if (event.getAction() == 1 && flag) {
-                    try {
-                        method.invoke(etTitle, true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            if (event.getAction() == 0) {
+                flag = true;
+            } else if (event.getAction() == 2) {
+                flag = false;
+                try {
+                    method.invoke(etTitle, false);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                return false;
+            } else if (event.getAction() == 1 && flag) {
+                try {
+                    method.invoke(etTitle, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            return false;
         });
     }
 
@@ -171,11 +151,14 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
         public void onClick(View v) {
             System.out.println("onclick.............");
             String text = textView.getText().toString();
-            ClipboardManager manager = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData mClipData = ClipData.newPlainText("Label", text);
-            manager.setPrimaryClip(mClipData);
-            Toast.makeText(getContext(), text + " 已复制成功", Toast.LENGTH_SHORT).show();
-            Log.i("xxx", "onClick: " + text);
+            if (null != requireContext().getSystemService(Context.CLIPBOARD_SERVICE)) {
+                ClipboardManager manager = (ClipboardManager) requireContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData mClipData = ClipData.newPlainText("Label", text);
+                manager.setPrimaryClip(mClipData);
+                Toast.makeText(getContext(), text + " 已复制成功", Toast.LENGTH_SHORT).show();
+                Log.i("xxx", "onClick: " + text);
+            }
+
         }
     }
 
@@ -188,22 +171,18 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
             View view = LayoutInflater.from(getContext()).inflate(R.layout.uri_list_item, parent, false);
             return new UriHolder(view);
         }
-
         @Override
         public void onBindViewHolder(@NonNull @NotNull DetailFragment.UriHolder holder, int position) {
             String uri = uriList.get(position);
             holder.bind(uri);
         }
-
         @Override
         public int getItemCount() {
             return uriList.size();
         }
     }
-
-
     private void updateUri() {
-        adapter = new UriAdapter();
+        UriAdapter adapter = new UriAdapter();
         recyclerView.setAdapter(adapter);
     }
 
