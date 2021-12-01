b3e75ed5e477a57ffad6ce47bdb12b8710c55055
Ouanu <ouanu@foxmail.com>
2021-12-01 23:14:08 +0800

    WhyNote

--- app/src/main/java/com/moment/whynote/fragment/MenuFragment.java
+++ app/src/main/java/com/moment/whynote/fragment/MenuFragment.java
@@ -1,6 +1,5 @@
 package com.moment.whynote.fragment;
 
-import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -11,8 +10,6 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
