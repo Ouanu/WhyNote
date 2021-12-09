0e264d25f04349f6f9d0d77c7a399bb29fac5076
Ouanu <ouanu@foxmail.com>
2021-12-09 23:27:16 +0800

    WhyNote

--- app/src/main/java/com/moment/whynote/service/WNClient.java
+++ app/src/main/java/com/moment/whynote/service/WNClient.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,18 +18,19 @@ import java.net.Socket;
 
 public class WNClient {
     private static WNClient mClient = null;
+    private static final String TAG = "WNClient.class";
 //    private Socket socket;
 //    private OutputStream os;
 
     public WNClient(String ipAddress, int port) throws IOException {
         Socket socket = new Socket(ipAddress, port);
-//        OutputStream os = socket.getOutputStream();
-//        PrintWriter pw = new PrintWriter(os);
-        //获取客户端ip地址
-//        InetAddress address = InetAddress.getLocalHost();
-//        String ip = address.getHostAddress();
-//        pw.write("客户端：" + ip + "接入服务器");
-//        pw.flush();
+        OutputStream os = socket.getOutputStream();
+        PrintWriter pw = new PrintWriter(os);
+//        获取客户端ip地址
+        InetAddress address = InetAddress.getLocalHost();
+        String ip = address.getHostAddress();
+        pw.write("客户端：" + ip + "接入服务器");
+        pw.flush();
 //        socket.shutdownOutput();
         sendFile(socket);
         socket.close();
@@ -45,9 +47,12 @@ public class WNClient {
     }
 
     public void sendFile(Socket socket) {
+//        File[] fileList = getFileList();
+//        Log.d(TAG, "sendFile: " + fileList.length);
         try {
+//            for (File file : fileList) {
             // 获得需要传输的文件
-            FileInputStream fs = new FileInputStream("/data/data/com.moment.whynote/databases");
+            FileInputStream fs = new FileInputStream(new File("/data/data/com.moment.whynote/databases/RES_DATABASE.db"));
             // 创建本地流
             BufferedInputStream bi = new BufferedInputStream(fs);
 
@@ -67,19 +72,30 @@ public class WNClient {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String str;
-            while ((str = br.readLine()) != null){
+            while ((str = br.readLine()) != null) {
                 System.out.println(str);
             }
-
             // 释放资源
-            socket.close();
             os.close();
             bi.close();
+
+            socket.close();
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public File[] getFileList() {
+        File file = new File("/data/data/com.moment.whynote/databases");        //获取其file对象
+        File[] fs = file.listFiles();    //遍历path下的文件和目录，放在File数组中
+        for (File f : fs) {                    //遍历File[]数组
+            if (!f.isDirectory())        //若非目录(即文件)，则打印
+                System.out.println(f);
+        }
+        return fs;
     }
 
 }
