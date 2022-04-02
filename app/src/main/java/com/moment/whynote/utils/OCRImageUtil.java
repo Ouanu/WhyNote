21a4ba6daa80cecacf650c1db6fdbd4212fbc4ab
Ouanu <ouanu@foxmail.com>
2022-04-02 21:53:59 +0800

    更改图片处理策略，提高识别率

--- app/src/main/java/com/moment/whynote/utils/OCRImageUtil.java
+++ app/src/main/java/com/moment/whynote/utils/OCRImageUtil.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 
 import android.provider.MediaStore;
@@ -13,18 +14,22 @@ import android.provider.MediaStore;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.opencv.core.CvType.CV_8U;
 import static org.opencv.imgproc.Imgproc.COLOR_RGB2GRAY;
 import static org.opencv.imgproc.Imgproc.HoughLines;
 import static org.opencv.imgproc.Imgproc.LINE_AA;
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.THRESH_OTSU;
 import static org.opencv.imgproc.Imgproc.getRotationMatrix2D;
 import static org.opencv.imgproc.Imgproc.line;
 import static org.opencv.imgproc.Imgproc.threshold;
@@ -96,55 +101,29 @@ public class OCRImageUtil {
         Mat gray = new Mat();
         Utils.bitmapToMat(bitmap, rgb);
 //        //降噪
-//        fastNlMeansDenoisingColored(rgb,mat,3,3,7,21);
+//        fastNlMeansDenoisingColored(rgb,rgb,3,3,7,21);
+
+        // 图片锐化
+        List<Mat> rgbList = new ArrayList<>();
+        Core.split(rgb, rgbList);
+
+        for (int i = 0; i < rgbList.size(); i++) {
+            Mat can = new Mat();
+            Mat finalMat = new Mat();
+            System.out.println("++++++++++++++++");
+            Imgproc.GaussianBlur(rgbList.get(i), can, new Size(3, 3), 0, 0);
+            Imgproc.Laplacian(can, finalMat, CV_8U);
+            Core.add(rgbList.get(i), finalMat, rgbList.get(i));
+        }
+        Core.merge(rgbList, rgb);
 
         Imgproc.cvtColor(rgb, gray, COLOR_RGB2GRAY);
-        Mat can = new Mat();
-
         //二值化
-        threshold(gray, gray, 100, 255, THRESH_BINARY);
+        threshold(gray, gray, 150, 255, THRESH_BINARY + THRESH_OTSU);
 
-        // 边缘提取+增强边缘
-        Imgproc.Canny(gray, can, 50, 200, 3);
-        Imgproc.Sobel(can, can, CV_8U, 1, 1,7);
-
-
-        //霍夫直线检测 (图片矫正)
-        Mat lines = new Mat();
-        HoughLines(can, lines, 1, Math.PI / 180, 200, 0, 0);
-        double sum = 0;
-        double angle = 0;
-        for (int i = 0; i < lines.rows(); i++) {
-            double[] vec = lines.get(i, 0);
-            double rho = vec[0];
-            double theta = vec[1];
-            Point pt1 = new Point(), pt2 = new Point();
-            double a = Math.cos(rho), b = Math.sin(theta);
-            double x = a * rho, y = b * theta;
-            pt1.x = Math.round(x + 1000 * (-b));
-            pt1.y = Math.round(y + 1000 * (a));
-            pt2.x = Math.round(x - 1000 * (-b));
-            pt2.y = Math.round(y - 1000 * (a));
-            sum += theta;
-            line(gray, pt1, pt2, new Scalar(55, 100, 195), 1, LINE_AA);
-            double average = sum / lines.rows();
-            angle = Math.toDegrees(average) - 90;
-        }
-
-        Point center = new Point();
-        center.x = rgb.cols() / 2.0;
-        center.y = rgb.rows() / 2.0;
-        int length = (int) Math.sqrt(rgb.cols()*rgb.cols() + rgb.rows()*rgb.rows());
-        System.out.println(angle);
-        Mat m = getRotationMatrix2D(center, angle, 1);
-        Mat src_rotate = new Mat();
-        //仿射变换，背景色填充为白色
-        warpAffine(rgb, src_rotate, m, new Size(length, length), 1, 0, new Scalar(255, 255, 255));
-
-        Bitmap finalbitmap = Bitmap.createBitmap(src_rotate.width(), src_rotate.height(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(src_rotate, finalbitmap);
+        Bitmap finalbitmap = Bitmap.createBitmap(gray.width(), gray.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(gray, finalbitmap);
         return finalbitmap;
-
     }
 
 }
